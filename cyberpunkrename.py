20d5a9941f3aca3d2ae517cc8a706fefc9acb1dc
aisling-krewer <94971377+aisling-krewer@users.noreply.github.com>
2025-01-27 21:20:04 +0000

    Update cyberpunkrename.py
    Updated to include words to separate out on files, to make them more readable in kavita.

--- cyberpunkrename.py
+++ cyberpunkrename.py
@@ -2,14 +2,26 @@ import os
 
 def rename_pdfs():
     # Strings to remove from filenames
-    strings_to_remove = ['RTG', 'DLC', 'CX']
-
+    # Will remove hardcoding at a later date
+    strings_to_remove = ['RTG', 'DLC', 'CX', '-', 'CPR', 'v1.11', 'v1.1', 'v1.0', 'v2', 'CEMK', 'LTR']
+    #Couldn't find a more straighforward way of defining words to be separated.
+    #Works great for my cyberpunk files, may need to rethink for other files
+    add_space = ['Cyberpunk', 'Blackhands', 'Black', 'All', 'About', 'Chrome', 'Street', 'Weapons', 'Breaking',
+                 'Your', 'Stuff', 'Chasing', 'the', 'Chromebook', '1', '2', 'and', '3', 'corporation', 'report',
+                 '2020', 'core', 'Cyberware', 'Danger', 'gal', 'Did', 'Someone', 'Say', 'Dreaded', 'Eurosource',
+                 'home', 'of', 'hope', 'reborn', 'Hornets', 'Interface', 'Vol', 'listen', 'Mixing', 'Drinks', 'Changing',
+                 'Night', 'No', 'Place', 'Like', 'Pacific', 'Rough', 'Guide', 'to', 'the', 'Screamsheet', 'Tales', 'Forlorn',
+                  'street', 'Toggles', 'Temple', 'TotR', 'New', 'Best', 'tales']
+    
     # Get the current working directory
+    # I'm calling this portable, not being lazy.
     directory = os.getcwd()
 
     # Iterate through all files in the directory
+    #TODO: This process is missing files within directories. 
     for filename in os.listdir(directory):
         # Check if the file is a PDF
+        # Focus is for Kavita library, may update later
         if filename.endswith('.pdf'):
             new_name = filename
             # Remove unwanted strings
@@ -17,6 +29,11 @@ def rename_pdfs():
                 new_name = new_name.replace(s, '')
             # Trim whitespace from the new name
             new_name = new_name.strip()
+            # Add spaces to the new name
+            for s in add_space:
+                #check s isn't right before or after a space, or just before .pdf
+                if s + ' ' not in new_name and ' ' + s not in new_name and s != new_name[-4:]:
+                    new_name = new_name.replace(s, s + ' ') 
             # Rename the file if the name has changed
             if new_name != filename:
                 original_path = os.path.join(directory, filename)
